76a1aee67c6e3b9a2d271ebc4bc15ec592f18dfe
CodeRed77 <rkato1@gmail.com>
2015-02-07 18:36:09 -0500

    Changes to output of graphics
    Changed method from dev.copy to
    png(file = "./plot3.png", width = 480, height = 480, units = "px")

--- plot3.R
+++ plot3.R
@@ -29,12 +29,12 @@ data2$Datetime <- as.POSIXct(datetime)
 
 
 ######Q3
-
+png(filename = "./plot3.png", width = 480, height = 480, units = "px")
+    
 plot(data2$Sub_metering_1 ~ data2$Datetime, type='l', xlab="", ylab="Energy sub metering")
 lines(data2$Sub_metering_2~ data2$Datetime, type='l', col='red')
 lines(data2$Sub_metering_3~ data2$Datetime, type='l', col='blue')
 legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1,
        col=c("black", "red", "blue"))
 
-dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
